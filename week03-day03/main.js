5f85161a225d7faba4a437cc3d8f0c75468b349d
Charles George van Oeffelen <charlevo@spartaglobal.co>
2017-03-01 16:05:46 +0000

    Event listener

--- week03-day03/main.js
+++ week03-day03/main.js
@@ -1,6 +1,6 @@
 console.log('In Main.js');
-
-console.log(document); //to get the HTML with in console, layout like the element tab
+document.addEventListener('DOMContentLoaded' , function () { 
+  console.log(document); //to get the HTML with in console, layout like the element tab
 
 //document.body
 //document.body.children
@@ -29,36 +29,56 @@ console.log(document); //to get the HTML with in console, layout like the elemen
 //
 
 
-console.log('selected List Items' , selectedListItems);
-
-console.log('_____creating a new li, giving it a class, and putting style on the select class');
-
-var newListItem = document.createElement('li');
 
 
-newListItem.innerHTML = 'Im New!';
-newListItem.className = 'selected';
+  console.log('_____creating a new li, giving it a class, and putting style on the select class');
+  console.log('selected List Items' , selectedListItems);
+  var newListItem = document.createElement('li');
+
+
+  newListItem.innerHTML = 'Im New!';
+  newListItem.className = 'selected';
 // newListItem.className = 'selected';
 
 
 
 //we are going to call .appendChild() on the parent <ul> element.
 //that <ul> eleent has an id attribute.
-var listContainer = document.getElementById('item-list');
+  var listContainer = document.getElementById('item-list');
 
-listContainer.appendChild(newListItem);
-
-
-
-var spaceDense = document.createElement('li');
-spaceDense.innerHTML = 'Wow Space things In DOM';
-listContainer.appendChild(spaceDense);
-
-var selectedListItems = document.querySelectorAll('li.selected');
+  listContainer.appendChild(newListItem);
 
 
-for(var i = 0 ; i < selectedListItems.length ; i++) {
-  console.log('selected list item:', selectedListItems[i].stlye);
-  selectedListItems[i].style.color = 'red';
-}
-console.log(newListItem);
+
+  var spaceDense = document.createElement('li');
+  spaceDense.innerHTML = 'Wow Space things In DOM';
+  listContainer.appendChild(spaceDense);
+
+  var selectedListItems = document.querySelectorAll('li.selected');
+
+
+  for(var i = 0 ; i < selectedListItems.length ; i++) {
+    console.log('selected list item:', selectedListItems[i].stlye);
+    selectedListItems[i].style.color = 'red';
+  }
+  console.log(newListItem);
+
+
+  console.log('____Dom events... css');
+
+  var touchme = document.getElementById('touch');
+
+  touchme.addEventListener('click' , function () {
+    touchme.innerHTML = 'something PG';
+    alert('Something also pg....');
+
+  });
+
+
+
+});
+//function handleButtonClick(){
+// alert('Clicked!')
+// document.getElementById('touch').innerHTML = 'Something PG';
+//}
+//touchme.addEventListener('click', handleButtonClick)
