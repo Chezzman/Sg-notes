fb41e1411e237cca3947b4f1c3987e9252d291df
Charles George van Oeffelen <charlesvo@spartaglobal.co>
2017-04-03 11:43:03 +0100

    adding starter code for week 8 day 1

---
+++ week08-day01-angular-directives/angular-directives/controllers/main.controller.js
@@ -0,0 +1,38 @@
+angular
+  .module('angularDirectives')
+  .controller('mainController', mainController);
+
+function mainController () {
+  var controller = this;
+
+  controller.people = [
+    {
+      name: 'Niall',
+      level: 12,
+      gender: 'male',
+      position: 'Instructor',
+      skills: ['eating', 'being awesome', 'sleeping']
+    },
+    {
+      name: 'Matt',
+      level: 69,
+      gender: 'male',
+      position: 'Instructor',
+      skills: ['Extreme Ping Pong', 'Angular']
+    },
+    {
+      name: 'Lexie',
+      level: 13,
+      gender: 'female',
+      position: 'Academy Manager',
+      skills: ['keeping the wheel spinning', 'life', 'knitting', 'danceing']
+    },
+    {
+      name: 'Steve',
+      level: 6,
+      gender: 'male',
+      position: 'Instructor/Education Author',
+      skills: ['Banter', 'life', 'keeping people motivated']
+    }
+  ];
+}
