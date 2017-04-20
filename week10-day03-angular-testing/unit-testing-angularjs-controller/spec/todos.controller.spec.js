ab8ac20ed58255b8b814d475c030f26d24bc8840
Charles George van Oeffelen <charlesvo@spartaglobal.co>
2017-04-20 10:22:53 +0100

    adding test for factory

--- week10-day03-angular-testing/unit-testing-angularjs-controller/spec/todos.controller.spec.js
+++ week10-day03-angular-testing/unit-testing-angularjs-controller/spec/todos.controller.spec.js
@@ -1,26 +1,84 @@
 
 describe('TodosController', () => {
   let controllerToTest;
+  let MockTodosFactory;
 
   beforeEach(() => {
     // naming the app
     module('todosApp');
+    MockTodosFactory = {
+      list: [],
+      add: jasmine.createSpy(),
+      clear: jasmine.createSpy()
+    };
 
+
+//creating the instance
     inject(($controller) => {
-      controllerToTest = $controller('TodosController');
+      controllerToTest = $controller('TodosController', {TodosFactory: MockTodosFactory});
       //calling the controller before using it
     });
   });
 
   describe('initialisation', () => {
-    it('Sahould set inputText correctl', () => {
-      expect(controllerToTest.inputText).toEqual('');
+    it('Should initialise a list correctly', () => {
+      expect(controllerToTest.list).toEqual([]);
       //checking to see if ths work
     });
 
-    it('Should set inputText correctly', () => {
+    it('Should initialise inputText correctly', () => {
 
+      expect(controllerToTest.inputText).toEqual('');
+
+    });
+  });
+
+  describe('add()', () => {
+    it('should call todos factory.add()with correct parameter',  () => {
+      const inputText = 'new todo';
+
+      controllerToTest.inputText = inputText;
+      controllerToTest.add();
+      expect(MockTodosFactory.add).toHaveBeenCalledWith(inputText);
+
+    });
+    it('should clear inputText', () => {
+      const inputText = 'new todo 2';
+
+      controllerToTest.inputText = inputText;
+      controllerToTest.add();
+      expect(controllerToTest.inputText).toEqual('');
+    });
+  });
+  describe('isSubmitButtonDisabled()', () => {
+    it('should return false when inputText is not empty', () => {
+    //returns false when there is text inside
+      controllerToTest.inputText = 'task';
+      expect(!controllerToTest.inputText).toBe(false);
+    });
+    it('should return true when inputText is empty', () => {
+      controllerToTest.inputText = '';
+      expect(!controllerToTest.inputText).toBe(true);
+    });
+    //returns true when there is no text inside
+
+  });
+
+  describe('isClearButtonDisabled()', () => {
+    it('should return false when task list is empty', () => {
+      controllerToTest.list = [];
       expect(controllerToTest.list).toEqual([]);
+
+    });
+    it('should return ture when task list is not empty', () => {
+      controllerToTest.list = ['I have to many tasks', 'complete  all the tasks'];
+      expect(controllerToTest.list).toEqual(controllerToTest.list);
+    });
+  });
+  describe('clear()', () => {
+    it('should call todo Factory.clear()', () => {
+      controllerToTest.clear();
+      expect(MockTodosFactory.clear).toHaveBeenCalledWith();
 
     });
   });
---
+++ week10-day03-angular-testing/unit-testing-angularjs-controller/spec/todos.factory.spec.js
@@ -0,0 +1,38 @@
+describe('TodosFactory', () => {
+  let factoryToTest;
+
+  beforeEach(module('todosApp'));
+
+  beforeEach(inject((TodosFactory) => {
+    factoryToTest = TodosFactory;
+  }));
+
+  describe('list', () => {
+    it('Should show a list of an emply array', () => {
+      expect(factoryToTest.list.length).toEqual(0);
+    });
+
+  });
+
+  describe('add()', () => {
+    it('Should add a new todo to the list', () => {
+      const newTodo = 'Wash cloths';
+      factoryToTest.add(newTodo);
+      expect(factoryToTest.list).toContain(newTodo);
+    });
+    it('Should add a new todo item to an empty todo list', () => {
+      const newTodo = 'Do shopping';
+      factoryToTest.add(newTodo);
+      var arrayList = 0;
+      expect(factoryToTest.list[arrayList]).toEqual(newTodo);
+    });
+  });
+
+  describe('clear()', () => {
+    it('Should clear the todo list', () => {
+      factoryToTest.list.push('Wash cloths', 'Do shopping');
+      factoryToTest.clear();
+      expect(factoryToTest.list.length).toEqual(0);
+    });
+  });
+});
